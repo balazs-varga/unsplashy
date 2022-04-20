8980882a5c2e0dea906eb2dac8d709df8960fab7
Balázs Varga <balazsvarga91@gmail.com>
2022-04-20 12:23:16 +0200

    get images by search text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import axios from "axios";
+import React from "react";
 import SearchBar from "./components/SearchBar";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+const BASE_UNSPLASH_API_URL = 'https://api.unsplash.com';
+
+class App extends React.Component {
+
+  state = { images: [] };
+
+  onSearchSubmit = async (searchText) => {
+    const response = await axios.get(`${BASE_UNSPLASH_API_URL}/search/photos`, {
+      params: { query: searchText },
+      headers: {
+        Authorization: `Client-ID ${UNSPLASH_API_KEY}`
+      }
+    });
+
+    this.setState({ images: response.data.results });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar userSubmit={this.onSearchSubmit} />
+        <span>Found: {this.state.images.length} images</span>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+
+    state = { searchText: ''};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.userSubmit(this.state.searchText);
+    }
+
+    onSearchInputChange = (event) => {
+        this.setState({ searchText: event.target.value });
+    }
+
     render() {
         return (
             <div>
-                <form className='flex-container'>
+                <form onSubmit={this.onFormSubmit} className='flex-container'>
                     <label htmlFor='search'>
                         <h2>Image search:</h2>
                     </label>
-                    <input type="text" className='search-input' />
+                    <input 
+                        value={this.state.searchText} 
+                        onChange={this.onSearchInputChange} 
+                        type="text" 
+                        className='search-input' 
+                    />
                 </form>
             </div>
         )
